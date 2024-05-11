4c11ebe54943e95ccc6fef074d3814bc37469ad6
sharif23pak <160602479+sharif23pak@users.noreply.github.com>
2024-05-11 10:40:19 +0500

    passwaord genreted

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById('generate').addEventListener('click', function() {
+    var length = document.getElementById('length').value;
+    var uppercase = document.getElementById('uppercase').checked;
+    var lowercase = document.getElementById('lowercase').checked;
+    var numbers = document.getElementById('numbers').checked;
+    var symbols = document.getElementById('symbols').checked;
+  
+    var password = generatePassword(length, uppercase, lowercase, numbers, symbols);
+    document.getElementById('password').value = password;
+  });
+  
+  function generatePassword(length, uppercase, lowercase, numbers, symbols) {
+    var charset = '';
+    if (uppercase) charset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (lowercase) charset += 'abcdefghijklmnopqrstuvwxyz';
+    if (numbers) charset += '0123456789';
+    if (symbols) charset += '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+    
+    var password = '';
+    for (var i = 0; i < length; i++) {
+      var randomIndex = Math.floor(Math.random() * charset.length);
+      password += charset[randomIndex];
+    }
+    return password;
+  }
+
+ 
